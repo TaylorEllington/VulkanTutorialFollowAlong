6b7347095b9bba1777c66bfc7e6bcbf61fbb9071
Taylor Ellington <taylor.ellington@intel.com>
2018-09-06 17:14:48 -0700

    vertex buffer

--- shaders/compile.bat
+++ shaders/compile.bat
@@ -1,3 +1,14 @@
-C:/VulkanSDK/1.0.61.1/Bin/glslangValidator.exe -V shader.vert
-C:/VulkanSDK/1.0.61.1/Bin/glslangValidator.exe -V shader.frag
-pause
+@echo off
+
+copy vert.hlsl shader.vert
+copy frag.hlsl shader.frag
+
+..\Libraries\VulkanSDK\Bin32\glslangValidator.exe -V shader.vert -o vert.spv
+..\Libraries\VulkanSDK\Bin32\glslangValidator.exe -V shader.frag -o frag.spv
+
+del shader.vert
+del shader.frag
+
+
+
+pause 
